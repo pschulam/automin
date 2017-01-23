0d63a6acddfd855da25ce8c2c3431a9527f0b790
Peter Schulam <pschulam@gmail.com>
2017-01-23 11:52:56 -0500

    Add callback kw arg.

--- automin/minimize.py
+++ automin/minimize.py
@@ -6,7 +6,7 @@ from scipy import optimize
 from autograd.util import flatten_func
 
 
-def minimize(func, init_params, method='BFGS'):
+def minimize(func, init_params, method='BFGS', callback=None):
     """Minimize a function with respect to its inputs."""
 
     grad = autograd.grad(func)
@@ -14,7 +14,7 @@ def minimize(func, init_params, method='BFGS'):
     flattened_func, _, _ = flatten_func(func, init_params)
     flattened_grad, unflatten, x = flatten_func(grad, init_params)
 
-    callback = _make_callback(flattened_func)
+    callback = _make_callback(flattened_func) if callback is None else callback
 
     result = optimize.minimize(
         flattened_func, x, jac=flattened_grad, method=method, callback=callback)
